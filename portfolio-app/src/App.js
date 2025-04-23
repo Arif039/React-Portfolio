23ac67cfe73a43d6eddb4e6361048310c34e1776
Arif039 <saif039@gmail.com>
2025-04-23 19:56:57 +0600

    banner

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Navbar from './components/navbar';
+import Banner from './components/banner';
 
 function App() {
   return (
 
     <>
       <Navbar/>
+      <Banner/>
     </>
     
   );
--- portfolio-app/src/components/banner.js
+++ portfolio-app/src/components/banner.js
@@ -1,8 +1,50 @@
-
+import React from 'react';
+import portfolioImage from '../images/portfolio_image-removebg-preview.png';
 
 export default function Banner () {
 
     return (
-        
+        <div className='w-5/6 m-auto flex flex-row'>
+          
+            {/* Content */}
+            <div className='flex flex-col gap-6'>
+                <div>
+                    <h1 className='font-bold text-7xl w-1/2'>Hello I'm Kazi Saiful Anwar </h1>
+                </div>
+
+                <div>
+                    <h3 className='text-3xl w-1/2'>I am a fullstack Blockchain Developer based on Bangladesh | Focused on building user-centric solutions across smart contracts, NFTs, and Defi platform </h3>
+                </div>
+                <div>
+                    <button href= "#" className='text-2xl'>Say Hello!</button>
+                </div>
+
+                <div className='flex flex-row gap-10'>
+
+                    <div>
+                        <h1>1 Y.</h1>
+                        <h3>Experience</h3>
+                    </div>
+
+                    <div>
+                        <h1>25+</h1>
+                        <h3>Project Completed</h3>
+                    </div>
+
+                    <div>
+                        <h1>19</h1>
+                        <h3>Happy Client</h3>
+                    </div>
+
+                </div>
+            
+                
+            </div>
+
+            {/* Image */}
+            <div className='w-2/6 grow'> 
+                <img src={portfolioImage} alt='banner_image' className='max-w-lg max-h-96'></img>
+            </div>
+        </div>
     )
 }
